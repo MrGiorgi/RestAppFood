d49c6313adcf4df6849a859a6ad55f2a6ddfa0e7
MrGiorgi <giorgisosa02@gmail.com>
2024-04-03 16:10:01 -0400

    Update!

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -4,12 +4,17 @@ import Trash from "@/components/icons/Trash";
 import AddressInputs from "@/components/layout/AddressInputs";
 import SectionHeaders from "@/components/layout/SectionHeaders";
 import CartProduct from "@/components/menu/CartProduct";
+import { useSession } from "next-auth/react";
+import {redirect} from "next/navigation";
 import { useProfile } from "@/components/UseProfile";
 import Image from "next/image";
 import { useContext, useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
 export default function CartPage() {
+  const session = useSession();
+  const { status } = session;
+
   const { cartProducts, removeCartProduct } = useContext(CartContext);
   const [address, setAddress] = useState({});
   const { data: profileData } = useProfile();
@@ -72,61 +77,74 @@ export default function CartPage() {
     });
   }
 
-  if (cartProducts?.length === 0) {
-    return (
-      <section className="mt-8 text-center">
-        <SectionHeaders mainHeader="Carrito de pedidos" />
-        <p className="mt-4">Su carrito está vacío... 😔</p>
-      </section>
-    );
-  }
 
-  return (
-    <section className="mt-8">
-      <div className="text-center">
-        <SectionHeaders mainHeader="Carrito de pedidos" />
-      </div>
-      <div className="mt-8 grid gap-8 grid-cols-2">
-        <div className="overflow-y-scroll p-2 bg-gray-200" style={{maxHeight:'calc(100vh - 100px)'}}>
-          {cartProducts?.length === 0 && (
-            <div>No hay productos en su carrito de compras.</div>
-          )}
-          {cartProducts?.length > 0 &&
-            cartProducts.map((product, index) => (
-              <CartProduct
-                key={index}
-                product={product}
-                index={index} // Pass the index to the CartProduct component
-                onRemove={removeCartProduct}
-              />
-            ))}
-          <div className="py-2 pr-16 flex justify-end items-center">
-            <div className="text-gray-500">
-              Subtotal:
-              <br />
-              Delivery:
-              <br />
-              Total:
+
+  if (status === "authenticated") {
+    if (cartProducts?.length === 0) {
+      return (
+        <section className="mt-8 text-center">
+          <SectionHeaders mainHeader="Carrito de pedidos" />
+          <p className="mt-4">Su carrito está vacío... 😔</p>
+        </section>
+      );
+    }
+       
+    return (
+      <>
+        <section className="mt-8">
+          <div className="text-center">
+            <SectionHeaders mainHeader="Carrito de pedidos" />
+          </div>
+          <div className="mt-8 grid gap-8 grid-cols-2">
+            <div
+              className="overflow-y-scroll p-2 bg-gray-200"
+              style={{ maxHeight: "calc(100vh - 100px)" }}
+            >
+              {cartProducts?.length === 0 && (
+                <div>No hay productos en su carrito de compras.</div>
+              )}
+              {cartProducts?.length > 0 &&
+                cartProducts.map((product, index) => (
+                  <CartProduct
+                    key={index}
+                    product={product}
+                    index={index} // Pass the index to the CartProduct component
+                    onRemove={removeCartProduct}
+                  />
+                ))}
+              <div className="py-2 pr-16 flex justify-end items-center">
+                <div className="text-gray-500">
+                  Subtotal:
+                  <br />
+                  Delivery:
+                  <br />
+                  Total:
+                </div>
+                <div className="font-semibold pl-2 text-right">
+                  ${subtotal}
+                  <br />
+                  $5
+                  <br />${subtotal + 5}
+                </div>
+              </div>
             </div>
-            <div className="font-semibold pl-2 text-right">
-              ${subtotal}
-              <br />
-              $5
-              <br />${subtotal + 5}
+            <div className="bg-gray-100 p-4 rounded-lg">
+              <h2>Verificar</h2>
+              <form onSubmit={proceedToCheckout}>
+                <AddressInputs
+                  addressProps={address}
+                  setAddressProp={handleAddressChange}
+                />
+                <button type="submit">Pagar ${subtotal + 5}</button>
+              </form>
             </div>
           </div>
-        </div>
-        <div className="bg-gray-100 p-4 rounded-lg">
-          <h2>Verificar</h2>
-          <form onSubmit={proceedToCheckout}>
-            <AddressInputs
-              addressProps={address}
-              setAddressProp={handleAddressChange}
-            />
-            <button type="submit">Pagar ${subtotal + 5}</button>
-          </form>
-        </div>
-      </div>
-    </section>
-  );
+        </section>
+      </>
+    );
+  }
+  if (status === "unauthenticated") {
+    return redirect('/login');
+  }
+
 }
